1b120d8375846c3da3673d50dc22f96532596c29
khui15 <69596358+khui15@users.noreply.github.com>
2023-07-08 23:51:11 +1000

    First commit at S10-159

---
+++ client/src/app/components/AppCheckbox.tsx
@@ -0,0 +1,24 @@
+import {  } from "@mui/icons-material";
+import { FormControlLabel, Checkbox } from "@mui/material";
+import { useController, UseControllerProps } from "react-hook-form";
+
+interface Props extends UseControllerProps {
+    label: string;
+}
+
+export default function AppCheckbox(props: Props) {
+    const {field} = useController({...props, defaultValue: false});
+
+    return (
+        <FormControlLabel
+            control= {
+                <Checkbox
+                    {...field}
+                    checked={field.value}
+                    color='secondary'
+                />
+            }
+            label={props.label}
+        />
+    )
+}
---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,58 @@
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useCallback, useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Header from "./Header";
+import 'react-toastify/dist/ReactToastify.css';
+import LoadingComponent from "./LoadingComponent";
+import { useAppDispatch } from "../store/configureStore";
+import { fetchBasketAsync, setBasket } from "../../features/basket/basketSlice";
+import { fetchCurrentUser } from "../../features/account/accountSlice";
+
+function App() {
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+
+  const initApp = useCallback(async () => {
+    try {
+      await dispatch(fetchCurrentUser());
+      await dispatch(fetchBasketAsync());
+    } catch (error: any) {
+      console.log(error);
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    initApp().then(() => setLoading(false));
+  }, [initApp])
+
+  const [darkMode, setDarkMode] = useState(false);
+  const palleteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default: (palleteType === 'light') ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
+  if (loading) return <LoadingComponent message="Initialising app..." />
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}  />
+      <Container>
+        <Outlet />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
